175d124cc539c9561e647e3a48991aa9ff27df63
coderchrisdean <coderchrisdean@gmail.com>
2023-01-18 13:33:27 -0800

    cleaned up code and routing

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 // api route
 
-const app = require('express').Router();
+const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
 const uuid = require('../helpers/uuid');
@@ -10,49 +10,59 @@ const uuid = require('../helpers/uuid');
 
     
 //GET `/api/notes` - Should read the db.json file and return all saved notes as JSON.
-app.get ('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-});
+
+    router.get('/api/notes', (req, res) => {
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        });
+    });
+
+
+
 
 //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-app.post ('/api/notes', (req, res) => {
-    let db = fs.readFileSync('../db/db.json', 'utf8');
-    db = JSON.parse(db);
-    res.json(db);
-    // create a new note
-    let userNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuid(),
-    };
-    // push new note to db.json
-    db.push(userNote);
-    // write new note to db.json
-    fs.writeFile('../db/db.json', JSON.stringify(db));
-    // return new note to client
-    res.json(userNote);
-    // log new note to console
-    console.log(userNote);
-    // log db.json to console
-    console.log(db);
-
-});
-
-//DELETE /api/notes/:id - receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
-app.delete ('/api/notes/:id', (req, res) => {
+router.post('/api/notes', (req, res) => {
     // read db.json file
-    let db = json.parse(fs.readFileSync('../db/db.json', 'utf8'));
-    // filter out note with matching id
-    db = db.filter(note => note.id !== req.params.id);
-    // write new db.json file
-    fs.writeFile('../db/db.json', JSON.stringify(db));
-    // return new db.json file
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        let db = JSON.parse(data);
+        // add new note to db.json file
+        let userNote = db.push(req.body);
+        // add unique id to note
+        userNote.id = uuid();
 
-    res.json(deleteNotes);
+        // write new db.json file
+        db.push(userNote);
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
+            if (err) throw err;
+            // return new db.json file
+            res.json(db);
+        });
+    });
+});
 
+
+// ** BONUS **
+
+router.delete ('/api/notes/:id', (req, res) => {
+//read db.json file by id
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        let db = JSON.parse(data);
+        //filter out the note with the id
+        let noteIndex = db.findIndex(note => note.id === req.params.id);   
+        //remove the note from the array
+        db.splice(noteIndex, 1);
+        //write new db.json file
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(db), (err) => {
+            if (err) throw err;
+            //return new db.json file
+            res.json(db);
+        });
+    });
 });
 
 
 
-module.exports = app
+module.exports = router
--- routes/html.js
+++ routes/html.js
@@ -1,19 +1,19 @@
 // This file handles the routing for the html pages
 //dependencies
 
-const app = require('express').Router();
+const router = require('express').Router();
 const path = require('path');
 
 //GET notes.html file
 
-app.get ('/notes', (req, res) => {
+router.get ('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 //wildcard route to serve index.html
-app.get ('*', (req, res) => {
+router.get ('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 //export app
-module.exports = app;
+module.exports = router;
