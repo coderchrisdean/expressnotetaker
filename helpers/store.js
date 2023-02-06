428004315592fd385d528cfd0b02c709125c11cd
coderchrisdean <coderchrisdean@gmail.com>
2023-02-05 17:58:02 -0800

    pasted store class in helpers/store.js to be used in apiRoutes to clean up code and DRY

--- helpers/store.js
+++ helpers/store.js
@@ -1,36 +1,47 @@
 //* My tutor Cody told me to copy and paste this code into this file. I did not write it.
 
-const fs = require('fs');
-const util = require('util');
+const util = require("util");
+const fs = require("fs");
+const uuid = require("../helpers/uuid");
 
-// Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
-const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync("db/db.json", "utf8")
     }
-  });
-};
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+    addNote(note) {
+        const { title, text } = note
+
+        if (!title || !text) {
+            throw new Error("title and text cannot be blank")
+        }
+
+        const newNote = { title, text, id: uuid() }
+
+        return this.getNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => this.newNote)
+    }
+
+    getNotes() {
+        return this.read()
+            .then(notes => {
+                return JSON.parse(notes) || [];
+            })
+    }
+    removeNote(id) {
+        return this.getNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(keptNotes => this.write(keptNotes))
+    }
+}
+
+module.exports = new Store();
+
